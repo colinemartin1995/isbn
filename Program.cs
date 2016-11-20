6b62fbddc0e8c0a97ee991647ff58c73e0f4a379
Coline Martin <Coline Martin>
2016-11-20 17:27:38 +0100

    Dev ISBN10

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ISBN10
 {
@@ -15,63 +16,49 @@ namespace ISBN10
 
             String message;
             String isbn;
+            String isbnSaisi;
+            bool isIsbnValid;
             String reponse;
             String blocExtrait;
            // int isbnNomnbre;
 
-
+            // Demander à l'utilisateur de saisir son ISBN10 ou ISBN13
             message = "Veuillez saisir votre ISBN : ";
-            Console.WriteLine(message);
-            isbn = Console.ReadLine();
-            reponse = "Votre ISBN est : " + isbn;
+            Console.WriteLine(message); 
 
+            // Récupération de l'ISBN saisi
+            isbnSaisi = Console.ReadLine();
          
-            blocExtrait = isbn.Replace("-", "");
-            Console.WriteLine(blocExtrait);
+            // Suppression des tirets éventuels dans l'ISBN saisi
+            isbn = isbnSaisi.Replace("-", "");
 
+            System.Text.RegularExpressions.Regex myRegex = new Regex(@"([0-9]){9}([0-9]|X|x)$");
+            isIsbnValid = myRegex.IsMatch(isbn); // retourne true ou false selon la vérification
 
-
-            if(blocExtrait.Length == 10)
+            if (!isIsbnValid)
             {
-               
-                String messagevalide;
-                messagevalide = "Votre ISBN est valide";
-                Console.WriteLine(messagevalide);
+                message = "L'ISBN n'est pas correct, veuillez saisir un ISBN valide ex : 2-8769-4033-7";
+                Console.WriteLine(message);
+                Program.Main(null); // Redemander la saisie de l'ISBN
+            }
+            else
+            {
+                int j = 1; // Initialisation du compteur pour la pondération
+                int somme = 0; // Initialisation de la somme pour le calcul de la clef
 
-                //isbnNomnbre = int.Parse(blocExtrait);
+                char[] isbnDecompose = isbn.ToCharArray(); // Création du tableau dans lequel on range chaque caractère de l'ISBN saisi
 
-                char[] array = blocExtrait.ToCharArray();
-                for (int i = 0; i < array.Length; i++)
-                {                  
-                    char number = array[i];
-                    Console.WriteLine(number);
+                // Parcourt du tableau du premier jusqu'à l'avant dernier élément 
+                for (int i = 0; i < isbnDecompose.Length - 1; i++)
+                {
+                    int number = (int)isbnDecompose[i];
+
+                    somme = somme + (number * j);
+                    j++; // ne pas oublier d'incrémenter le compteur pour la pondération
                 }
-
-
-
-
             }
 
-        
-
-            else
-             {
-                    Console.WriteLine(message);
-
-             }
-
-
-        
-       
-
-            //Char delimiter = '-';
-            //String[] substrings = reponse.Split(delimiter);
-            //foreach (var blocExtrait in substrings)
-            //Console.WriteLine(blocExtrait);
-
-
-
-
         }
+
     }
 }
